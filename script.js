cb1c54c5f80745df55f4f8c8ba60fe8974a42cf0
panyokkkkk <panyokkkkk@gmail.com>
2026-09-02 11:29:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Portfolio Elvando Loaded");
+const text =
+"Web Development Enthusiast • XII RPL 1 • SMKN 1 Semparuk";
+
+let i = 0;
+
+function typingEffect(){
+
+    if(i < text.length){
+
+        document.getElementById("typing").innerHTML += text.charAt(i);
+
+        i++;
+
+        setTimeout(typingEffect, 60);
+    }
+}
+
+window.onload = typingEffect;
